b71189834cbe5cf526b30305bf1244459350fbd0
jm.kim <>
2023-07-25 17:59:00 +0900

    backup

--- app/src/main/java/com/example/diarybycompose/ui/MainDescription.kt
+++ app/src/main/java/com/example/diarybycompose/ui/MainDescription.kt
@@ -1,5 +1,6 @@
 package com.example.diarybycompose.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,10 +12,14 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.diarybycompose.R
 import com.example.diarybycompose.data.ItemEntity
 import com.example.diarybycompose.ui.theme.DiaryByComposeTheme
 
@@ -38,11 +43,17 @@ fun MyDiaryList(items: ItemEntity, modifier: Modifier = Modifier) {
 
 @Composable
 fun GridItem(item: Int) {
-    Box() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(painter = painterResource(R.drawable.facebook), contentDescription = "MyDiaryImage")
+
         Text(
-            text =  item.toString(),
+            text = item.toString(),
             color = Color.Black,
-            modifier = Modifier.padding(8.dp)
+            modifier = Modifier.padding(8.dp),
+            textAlign = TextAlign.Center
         )
     }
 }
