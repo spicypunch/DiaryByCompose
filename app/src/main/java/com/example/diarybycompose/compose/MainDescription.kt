07e7f2c812ed313b14cf23265945089b4a2d28fb
jm.kim <>
2023-07-27 09:00:20 +0900

    Add 버튼 추가

--- app/src/main/java/com/example/diarybycompose/compose/MainDescription.kt
+++ app/src/main/java/com/example/diarybycompose/compose/MainDescription.kt
@@ -9,6 +9,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,8 +23,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.diarybycompose.R
-import com.example.diarybycompose.data.ItemEntity
 import com.example.diarybycompose.compose.theme.DiaryByComposeTheme
+import com.example.diarybycompose.data.ItemEntity
 
 
 @Composable
@@ -46,6 +50,17 @@ fun MainDescription() {
             verticalArrangement = Arrangement.SpaceBetween
         ) {
             MyDiaryList(list)
+        }
+        FloatingActionButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.BottomEnd)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = "Add",
+            )
         }
     }
 }
